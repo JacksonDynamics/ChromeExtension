3c9c1bef3d564e4eae8d6af8c00c4796494d2383
studmanj <bjackso3@hotmail.com>
2022-08-13 14:21:26 -0600

    Parse and Stringify

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let myLeads = `["www.awesomePossum.com"]`
 
+// 1. Turn the myLeads string into an array
 myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+myLeads.push("www.lead2.com")
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
 
-myLeads.push("www.epicWeb.com")
+
+
+
 
 
 const inputEl = document.getElementById('input-el')
