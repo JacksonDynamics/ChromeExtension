08e166c45444296c0ab9fee4b9896a7cac49a352
studmanj <bjackso3@hotmail.com>
2022-08-14 13:48:00 -0600

    Save the leads to localStorage

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-let myLeads = `["www.awesomePossum.com"]`
-
-// 1. Turn the myLeads string into an array
-myLeads = JSON.parse(myLeads)
-// 2. Push a new value to the array
-myLeads.push("www.lead2.com")
-// 3. Turn the array into a string again
-myLeads = JSON.stringify(myLeads)
-// 4. Console.log the string using typeof to verify that it's a string
-console.log(typeof myLeads)
-
-
-
-
-
-
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 let inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-localStorage.setItem("myLeads, www.hotmal.com")
-localStorage.getItem("myLeads")
 
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+
+    console.log( localStorage.getItem("myLeads") )
 })
 
 
@@ -40,7 +26,6 @@ function renderLeads() {
                 </a>
             </li>
         `       
-        console.log(listItems)
     }
     
     ulEl.innerHTML = listItems
